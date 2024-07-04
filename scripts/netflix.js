babcb5d39097082eff7403d748a018bdc413117d
ImEldie <daniel.frankort@telenet.be>
2024-07-04 14:14:12 +0200

    linked settings to netflix.js

---
+++ scripts/netflix.js
@@ -0,0 +1,93 @@
+// Init & Config
+console.log("BingeFlix --- Detected Netflix");
+const defaultTimeout = 2; // => seconds until button press (default = 3s)
+const timeInterval = 1000;
+
+let buttonConfigs = [
+  {
+    dataUia: "player-skip-intro",
+    timeout: defaultTimeout,
+    description: "Skip Intro",
+    syncStorageName: "autoSkipIntro",
+    enabled: true,
+  },
+  {
+    dataUia: "next-episode-seamless-button",
+    timeout: defaultTimeout,
+    description: "Next Episode",
+    syncStorageName: "autoNextEpisode",
+    enabled: true,
+  },
+];
+
+// Functions
+function initButtonConfig() {
+  for (let config of buttonConfigs) {
+    loadButtonSettings(config, true);
+  }
+}
+
+async function loadButtonSettings(config, withLogging = false) {
+  config.enabled = await _loadEnabled(config.syncStorageName);
+
+  if (config.enabled === undefined || typeof config.enabled !== "boolean") {
+    if (withLogging) {
+      console.warn(
+        `BingeFlix --- ${config.syncStorageName} - Unknown value \'${config.enabled}\', defaulting to TRUE`
+      );
+    }
+    config.enabled = true;
+  } else if (withLogging) {
+    console.log(`BingeFlix --- ${config.syncStorageName} - ${config.enabled}`);
+  }
+}
+
+function _loadEnabled(storageName) {
+  const promise = new Promise((resolve, reject) => {
+    chrome.storage.sync.get([storageName], (result) => {
+      // On error
+      if (chrome.runtime.lastError) {
+        return reject(chrome.runtime.lastError);
+      }
+      // When data is received
+      const setting = Object.values(result)[0];
+      resolve(setting);
+    });
+  });
+
+  return promise.then((response) => {
+    return response;
+  });
+}
+
+function findAndClickButton(config) {
+  const queryName = `button[data-uia="${config.dataUia}"]`;
+  const button = document.querySelector(queryName);
+
+  if (button) {
+    loadButtonSettings(config);
+    if (config.enabled) {
+      if (config.timeout <= 0) {
+        button.click();
+        console.log(`BingeFlix --- Pressed ${config.description} button`);
+      } else {
+        button.classList.add("bingeflix-target");
+      }
+      config.timeout--;
+    }
+  } else {
+    config.timeout = defaultTimeout;
+  }
+}
+
+// Main loop
+initButtonConfig();
+setInterval(() => {
+  buttonConfigs.forEach((config) => findAndClickButton(config));
+}, timeInterval);
+
+setInterval(() => {
+  console.log("reloading settings");
+  buttonConfigs.forEach((config) => loadButtonSettings(config));
+  console.log(buttonConfigs);
+}, 60000);
